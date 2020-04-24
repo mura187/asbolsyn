e7ba377f2d1543909e61de9738155a8589a20245
mura187 <muratseit@gmail.com>
2020-04-25 01:34:35 +0500

    feat(geolocation): add showing geolocation and radius 1km

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import itemActions from 'src/store/item/actions';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { YMaps, Map, Placemark, Circle } from 'react-yandex-maps';
 import SearchToggler from 'src/components/atoms/SearchToggler';
 import TabBar from 'src/components/molecules/TabBar';
+import PlacemarkIcon from 'src/assets/images/icons/placemark.svg';
 import { MapPageTypes } from './types';
 
 function MapPage(props: MapPageTypes.IProps) {
   const { places, getItems } = props;
-  const coordinates = [50.322687, 57.131267];
   const [didMount, setDidMount] = useState(false);
+  const [latitude, setLatitude] = useState(50.322687);
+  const [longitude, setLongitude] = useState(57.131267);
+  const coordinates = [latitude, longitude];
+  const SEARCH_RADIUS_IN_METERS = 1000;
+
+  const getMyLocation = () => {
+    let location = null;
+    if (window.navigator && window.navigator.geolocation) {
+      location = window.navigator.geolocation;
+    }
+    if (location) {
+      location.getCurrentPosition((position: any) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude) ;
+      });
+    }
+  };
 
   useEffect(() => {
     if (!didMount) {
+      getMyLocation();
       setDidMount(true);
       getItems && getItems();
     }
@@ -20,7 +38,7 @@ function MapPage(props: MapPageTypes.IProps) {
   [didMount, getItems],
   );
 
-  const mapState = { center: { ...coordinates }, zoom: 11 };
+  const mapState = { center: { ...coordinates }, zoom: 14 };
 
   const mapModules = [
     'templateLayoutFactory',
@@ -29,7 +47,6 @@ function MapPage(props: MapPageTypes.IProps) {
     'geoObject.addon.balloon',
     'control.ZoomControl',
   ];
-
   return (<>
       <SearchToggler link="/" title="К списку" />
       <YMaps>
@@ -38,8 +55,27 @@ function MapPage(props: MapPageTypes.IProps) {
             suppressMapOpenBlock: true,
           }}
         >
-          {places && places.data.map((n: any) =>
+          <Circle
+            geometry={[
+              [latitude, longitude],
+              SEARCH_RADIUS_IN_METERS,
+            ]}
+            options={{
+              fillColor: '#00BBFF',
+              strokeColor: '#00BBFF',
+              fillOpacity: 0.1,
+              strokeOpacity: 0.5,
+              strokeWidth: 3,
+            }}
+          />
+          {places && places.data.map((n: any) => <>
             <Placemark
+              options={{
+                iconLayout: 'default#image',
+                iconImageHref: PlacemarkIcon,
+                iconImageSize: [30, 42],
+                iconImageOffset: [-3, -42],
+              }}
               geometry={n.location}
               defaultProperties={{
                 balloonContentBody: `
@@ -47,7 +83,8 @@ function MapPage(props: MapPageTypes.IProps) {
                   <i>${n.location}</i>
                 `,
               }}
-            />,
+            />
+          </>,
           )}
         </Map>
       </YMaps>
