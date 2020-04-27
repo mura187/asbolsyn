9f53bd05109bb6e1cadd041e0e8d33f58f600d9b
mura187 <muratseit@gmail.com>
2020-04-27 21:43:50 +0500

    test: make requests as https

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -2,11 +2,11 @@ const env = process.env.NODE_ENV;
 export const API_URL = (
   env === 'production' ?
     // prod
-    'https://52.58.74.111:8080/'
+    'https://0.0.0.0:8080/'
   : env === 'development' ?
     // dev
-    'https://52.58.74.111:8080/'
+    'https://0.0.0.0:8080/'
   :
     // local
-    'https://52.58.74.111:8080/'
+    'https://0.0.0.0:8080/'
 );
